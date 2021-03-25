9e8e57fd28a2e36d4c18019d603d7593ae0e36ed
Yoshiya Hinosawa <stibium.121@gmail.com>
2021-03-25 12:36:10 +0900

    Update myhomepage.tsx

--- myhomepage.tsx
+++ myhomepage.tsx
@@ -14,7 +14,7 @@ homepage(() => {
           style="max-width: 120px; border-radius: 100%;"
         />
         <h2>Yoshi</h2>
-        <a href="mailto:yoshiya@deno.com">yoshita@deno.com</a>
+        <a href="mailto:yoshiya@deno.com">yoshiya@deno.com</a>
       </header>
       <div style="margin-top: 64px;">
         {Object.keys(PROJECTS).map((name) => (
