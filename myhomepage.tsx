3eac19d048638e77bc9a0127cfd8cb5f8656a13b
Yoshiya Hinosawa <stibium.121@gmail.com>
2021-03-25 12:27:05 +0900

    Update myhomepage.tsx

--- myhomepage.tsx
+++ myhomepage.tsx
@@ -1,8 +1,8 @@
 import { Fragment, h, homepage } from "./homepage/mod.tsx";
 
 const PROJECTS: Record<string, string> = {
-  Deno: "https://github.com/denoland/deno",
-  "dext.ts": "https://github.com/lucacasonato/dext.ts",
+  Deno: "https://github.com/capsidjs/capsid",
+  "dext.ts": "https://github.com/kt3k/lepont",
 };
 
 homepage(() => {
@@ -13,8 +13,8 @@ homepage(() => {
           src="https://avatars.githubusercontent.com/u/7829205"
           style="max-width: 120px; border-radius: 100%;"
         />
-        <h2>Luca Casonato</h2>
-        <a href="mailto:luca@deno.com">luca@deno.com</a>
+        <h2>Yoshi</h2>
+        <a href="mailto:yoshiya@deno.com">luca@deno.com</a>
       </header>
       <div style="margin-top: 64px;">
         {Object.keys(PROJECTS).map((name) => (
